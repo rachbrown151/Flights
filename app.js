4b81b155990d7911173410265f09cfcd4f7ff348
Rachel Brown <brown.rachel.94@gmail.com>
2016-03-03 21:52:56 -0500

    Slider changes bubble colors/sizes

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var flightData = [];
+var oneHourValue = [100, 50, 25, 15, 7.25];
+var metric = 3;
+var minTime;
+var minPriceForMinTime;
 
 var chart = new CanvasJS.Chart("chartContainer",
     {
@@ -10,9 +14,9 @@ var chart = new CanvasJS.Chart("chartContainer",
     axisX: {
       title:"Round Trip Flight Time (hours)",
       valueFormatString: "#0.#",
-      maximum: 32,
-      minimum: 15,
-      interval: 1,
+      maximum: 34,
+      minimum: 16,
+      interval: 2,
       gridThickness: 1,
       tickThickness: 1,
       gridColor: "lightgrey",
@@ -27,49 +31,89 @@ var chart = new CanvasJS.Chart("chartContainer",
       tickColor: "lightgrey",
       lineThickness: 0,
       valueFormatString:"#0.#",
-      minimum: 500,
-      maximum: 1000,
-      interval: 50
+      minimum: 400,
+      maximum: 1100,
+      interval: 100
     },
     data: [
     {
       fillOpacity: .7,
       type: "bubble",
-      toolTipContent: "<span style='\"'color: {color};'\"'><strong>{label}</strong></span><br/> <strong>Travel Time</strong> {x} hours <br/> <strong>Price</strong> ${y}<br/>",
+      toolTipContent: "<span style='\"'color: {color};'\"'><strong>{label}</strong></span><br/> <strong>Travel Time</strong> {x} hours <br/> <strong>Price</strong> ${y}<br/> <strong>Z:</z> {z}<br/>",
       dataPoints: []
     } ]
   });
 
 var initData = function() {
   $.getJSON('http://localhost:3000/data', function(data) {
-    for (var i = 0; i < data.length; i++) {
-      var z = parseInt(data[i].z);
-      var color = "green";
-      if ( z <= data.length / 3.0 ) {
-        color = "#c70000";
-      } else if ( z <= 2 * ( data.length / 3.0 ) ) {
-        color = "#FFEE2E";
-      }
-      flightData.push({ label: data[i].label, x: parseInt(data[i].x), y: parseInt(data[i].y), z: parseInt(data[i].z), color:color });
-    }
+    findMins(data);
+    findZ();
+    setColors();
   }).done(function() {
      chart.options.data[0].dataPoints = flightData;
      chart.render(); 
   });
 }
 
+var findMins = function(data) {
+  minTime = data[0].x;
+  minPriceForMinTime = data[0].y;
+  for (var i = 0; i < data.length; i++) {
+    var x = parseFloat(data[i].x);
+    var y = parseFloat(data[i].y);
+    if(x < minTime || (x === minTime && y < minPriceForMinTime)) {
+      minTime = x;
+      minPriceForMinTime = y;
+      console.log(minPriceForMinTime);
+    }
+    flightData.push({ label: data[i].label, x: x, y: y });
+  }
+}
+
+var findZ = function() {
+  for (var i = 0; i < flightData.length; i++) {
+    var extraTime = flightData[i].x - minTime;
+    var priceDecrease = minPriceForMinTime - flightData[i].y;
+    var z = priceDecrease - (oneHourValue[metric - 1] * extraTime);
+    flightData[i].z = z;
+  }
+}
+
+function compare(a,b) {
+  if (a.z < b.z)
+    return -1;
+  else if (a.z > b.z)
+    return 1;
+  else 
+    return 0;
+}
+
+var setColors = function() {
+  flightData.sort(compare);
+  for (var i = 0; i < flightData.length; i++) {
+    if (i < flightData.length/3) {
+      flightData[i].color = "#c70000";
+      flightData[i].z = (flightData[i].z/4.0);
+    } else if (i < (flightData.length/3)*2) {
+      flightData[i].color = "#FFEE2E";
+      flightData[i].z = (flightData[i].z/2.0);
+    } else {
+      flightData[i].color = "green";
+    }
+  }
+}
+
 var onInput = function(value) {
+  metric = value;
+  findZ();
+  setColors();
   $('#timemoney').text(value);
 }
 
 $('#timemoneyselector').on('input', function(){
     onInput(this.value);
+    chart.render();
 });
 
-var metricCalculator = function(value){
-  
-}
-
 $(document).ready(function() {
-  metricCalculator();
 });
